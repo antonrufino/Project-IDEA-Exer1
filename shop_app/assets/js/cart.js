f91d8d38ee1caaf8c7e4601864f681ae4e32f1c3
Anton Rufino <anton.vongola@gmail.com>
2016-07-24 00:04:48 +0800

    Finish backend

---
+++ shop_app/assets/js/cart.js
@@ -0,0 +1,48 @@
+$(document).ready(() => {
+    if (typeof Storage !== null) {
+        let total = 0;
+        let cart = localStorage.getItem("shopping-cart");
+
+        if (cart === null) return;
+
+        $.each(JSON.parse(cart), (index, data) => {
+            let html = `
+                <tr>
+                    <td>${data.name}</td>
+                    <td>${data.price}</td>
+                    <td>${data.amount}</td>
+                    <td>${data.price * data.amount}</td>
+                </tr>
+            `;
+
+            $("tbody").append(html);
+            total += data.price * data.amount;
+        });
+
+        $("#total").html("PHP " + total);
+    } else {
+        Materialize.toast("Local storage not supported. Update ka na beh.", 4000);
+    }
+
+    $("#pay-btn").click(() => {
+        if (typeof Storage !== null) {
+            localStorage.removeItem("shopping-cart");
+            $("tbody").html("");
+            $("#total").html("PHP 0.00");
+            Materialize.toast("Edi wow. Ikaw na bracket A beh.", 4000);
+        } else {
+            Materialize.toast("Local storage not supported. Update ka na beh.", 4000);
+        }
+    });
+
+    $("#cancel-btn").click(() => {
+        if (typeof Storage !== null) {
+            localStorage.removeItem("shopping-cart");
+            $("tbody").html("");
+            $("#total").html("PHP 0.00");
+            Materialize.toast("Edi wow beh.", 4000);
+        } else {
+            Materialize.toast("Local storage not supported. Update ka na beh.", 4000);
+        }
+    });
+});
--- shop_app/assets/js/product-puller2.js
+++ shop_app/assets/js/product-puller2.js
@@ -18,15 +18,18 @@ $(document).ready(() => {
                             </div>
                             <div class="card-action">
                                 In stock: ${data.quantity}
-                                <form>
+                                <form class="order-form">
                                     <div class="row">
                                         <div class="input-field col s12">
-                                            <label for="${data.name + '-key'}">Amount:</label>
-                                            <input id="${data.name + '-key'}" class="validate" type="number" name="amount" />
+                                            <label for="${'key' + index}">Amount:</label>
+                                            <input id="${'key' + index}" class="validate" type="number" name="amount" />
                                         </div>
+                                        <button class="btn waves-effect waves-light amber darken-3 white-text" type="button" name="action" style="margin-left: 10.5px;"
+                                            data-index="${index}" data-name="${data.name}" data-price="${data.price}" data-quantity="${data.quantity}">Add to
+                                            <i class="material-icons right">shopping_cart</i>
+                                        </button>
                                     </div>
                                 </form>
-                                <a href="#" class="buy-btn">Buy</a>
                             </div>
                         </div>
                     </div>
@@ -34,6 +37,39 @@ $(document).ready(() => {
 
                 $("#card-container > div.row").append(html);
             });
+
+            $(".order-form button").click(e => {
+                if (typeof Storage !== undefined) {
+                    let cart;
+                    let jsonCart = localStorage.getItem("shopping-cart");
+
+                    if (jsonCart === null || jsonCart === "") cart = [];
+                    else cart = JSON.parse(jsonCart);
+
+                    let order = {
+                        name: $(e.target).data("name"),
+                        price: $(e.target).data("price"),
+                        amount: $("#key" + $(e.target).data("index")).val()
+                    };
+
+                    if (order.amount > $(e.target).data("quantity")) {
+                        Materialize.toast("Not enough in stock for order. #ArayBeh", 4000);
+                        return;
+                    } else if (order.amount <= 0) {
+                        Materialize.toast("You ordered nothing. #SeryosoKaBaBeh", 4000);
+                        return;
+                    }
+
+                    cart.push(order);
+                    localStorage.setItem("shopping-cart", JSON.stringify(cart));
+
+                    Materialize.toast("Order added to cart. #EdiWowBeh", 4000);
+                    console.log(localStorage.getItem("shopping-cart"));
+                } else {
+                    Materialize.toast("Local storage not supported. Update ka na beh.", 4000);
+                }
+            });
+            $(".order-form button").submit(false);
         })
         .fail(() => {
             Materialize.toast("A server error happened. #ArayKoBeh", 4000);
